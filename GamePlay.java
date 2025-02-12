01c9f70e9d00eda683a7a828f9d3d52296dbada3
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2025-02-12 22:04:55 +0530

    create variable

--- GamePlay.java
+++ GamePlay.java
@@ -1,8 +1,27 @@
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
 import java.awt.event.KeyListener;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+public class GamePlay extends JPanel implements KeyListener, ActionListener{
+
+
+    private boolean play = false;
+    private int score = 0;
+
+    private int totalBricks = 21;
+
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
